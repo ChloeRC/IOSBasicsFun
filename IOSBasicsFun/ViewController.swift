3d2907d079b1cc794f91d885592bde74c5000858
Chloe Crawford <ccrawford3@zagmail.gonzaga.edu>
2020-09-23 11:50:34 -0700

    class end

--- IOSBasicsFun/ViewController.swift
+++ IOSBasicsFun/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
+// MARK: - Overview of an iOS project
+// the folders are not folders, they are groups
+// changes to the groups do not change on disk
+// iOSBasicsFun
+// - AppDelegate.swift: middleman between your app and the OS
+// Rest of notes on GitHub, Gina's version
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("hello from viewDidLoad()")
     }
 
 
